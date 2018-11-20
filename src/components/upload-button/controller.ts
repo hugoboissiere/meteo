e2cfa9049e4510631fd26693c12eb5598f6586f7
EL SEIFY Ramy <elseify.ramy@gmail.com>
2018-11-20 16:09:54 +0100

    correction magic number

--- src/components/upload-button/controller.ts
+++ src/components/upload-button/controller.ts
@@ -1,22 +1,23 @@
 import { DocumentPicker, DocumentPickerUtil, Result } from 'react-native-document-picker'
 import * as RNFS from "react-native-fs"
 
+const FILE_START = 2
+
 export function moveFile(url: string) {
-
 	RNFS.readFile(url).then((res) => {
+		const array = res.split('\n')
+		const document: String[][] = []
 
-		let array = res.split('\n');
-		let document: String[][] = [];
-		array.splice(0, 2);
+		array.splice(0, FILE_START)
 		array.forEach(element => {
-			const item = element.split('\t');
-			document.push(item);
-		});
-		console.log(document[0]);
-		const fileName = document[0][0].split(" ")[0];
-		const path = RNFS.ExternalDirectoryPath + '/' + fileName + '.his';
+			const item = element.split('\t')
+			document.push(item)
+		})
+
+		const fileName = document[0][0].split(" ")[0]
+		const path = RNFS.ExternalDirectoryPath + '/' + fileName + '.his'
 		RNFS.moveFile(url, path).then(() => {
-			console.log("File Moved to ", path);
+
 		})
 			.catch((err) => {
 				console.warn("Error: " + err.message)
